2b1f57132d5f549ea7f6677324a66141b8b5aa7e
Isdogmc <isaac.e.taylor@gmail.com>
2025-02-03 20:01:43 -0600

    more elevator and updated drive constants

--- src/main/java/frc/robot/subsystems/elevator/Elevator.java
+++ src/main/java/frc/robot/subsystems/elevator/Elevator.java
@@ -2,50 +2,78 @@ package frc.robot.subsystems.elevator;
 
 import static frc.robot.util.SparkUtil.*;
 
+import com.revrobotics.RelativeEncoder;
 import com.revrobotics.spark.SparkBase.PersistMode;
 import com.revrobotics.spark.SparkBase.ResetMode;
+import com.revrobotics.spark.SparkClosedLoopController;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
 import com.revrobotics.spark.SparkMax;
 import com.revrobotics.spark.config.ClosedLoopConfig.FeedbackSensor;
 import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
 import com.revrobotics.spark.config.SparkMaxConfig;
+import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import org.littletonrobotics.junction.AutoLogOutput;
 
 public class Elevator extends SubsystemBase {
-  private final SparkMax highElevatorMotor = new SparkMax(10, MotorType.kBrushless);
-  private final SparkMax lowElevatorMotor = new SparkMax(11, MotorType.kBrushless);
+  private final SparkMax leftElevatorMotor = new SparkMax(10, MotorType.kBrushless);
+  private SparkClosedLoopController leftClosedLoopController;
+  private RelativeEncoder elevatorEncoder = leftElevatorMotor.getEncoder();
+
+  private final SparkMax rightElevatorMotor = new SparkMax(11, MotorType.kBrushless);
+  private SparkClosedLoopController rightClosedLoopController;
+
+  @AutoLogOutput(key = "elevator/setPoint")
+  private Rotation2d elevatorSetpoint = new Rotation2d();
 
   public Elevator() {
-    var highSparkMaxConfig = new SparkMaxConfig();
+    var leftSparkMaxConfig = new SparkMaxConfig();
+    leftClosedLoopController = leftElevatorMotor.getClosedLoopController();
 
-    highSparkMaxConfig
+    leftSparkMaxConfig
         .idleMode(IdleMode.kBrake)
         .smartCurrentLimit(40, 40)
-        .secondaryCurrentLimit(50)
-        .inverted(sparkStickyFault);
+        .secondaryCurrentLimit(50);
 
-    highSparkMaxConfig.closedLoop.feedbackSensor(FeedbackSensor.kPrimaryEncoder).pid(0, 0, 0);
+    leftSparkMaxConfig.closedLoop.feedbackSensor(FeedbackSensor.kPrimaryEncoder).pid(0, 0, 0);
 
     tryUntilOk(
-        highElevatorMotor,
+        leftElevatorMotor,
         5,
         () ->
-            highElevatorMotor.configure(
-                highSparkMaxConfig,
+            leftElevatorMotor.configure(
+                leftSparkMaxConfig,
                 ResetMode.kResetSafeParameters,
                 PersistMode.kPersistParameters));
 
-    var lowSparkMaxConfig = new SparkMaxConfig();
+    var rightSparkMaxConfig = new SparkMaxConfig();
+    rightClosedLoopController = rightElevatorMotor.getClosedLoopController();
 
-    lowSparkMaxConfig.idleMode(IdleMode.kBrake).smartCurrentLimit(40, 40).secondaryCurrentLimit(50);
-
-    lowSparkMaxConfig.closedLoop.feedbackSensor(FeedbackSensor.kPrimaryEncoder).pid(0, 0, 0);
+    rightSparkMaxConfig.follow(10, true);
 
     tryUntilOk(
-        lowElevatorMotor,
+        rightElevatorMotor,
         5,
         () ->
-            lowElevatorMotor.configure(
-                lowSparkMaxConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters));
+            rightElevatorMotor.configure(
+                rightSparkMaxConfig,
+                ResetMode.kResetSafeParameters,
+                PersistMode.kPersistParameters));
   }
+
+  public void setShoulderSetpoint(Rotation2d setpoint) { // change values later
+    if (setpoint.getDegrees() < -5) {
+      setpoint = Rotation2d.fromDegrees(-5);
+    } else if (setpoint.getDegrees() > 100) {
+      setpoint = Rotation2d.fromDegrees(100);
+    }
+    elevatorSetpoint = setpoint;
+  }
+
+  @AutoLogOutput(key = "elevator/rotations")
+  public Rotation2d getShoulderAngle() {
+    return Rotation2d.fromRotations(elevatorEncoder.getPosition());
+  }
+
+  public void periodic() {}
 }
